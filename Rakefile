6a800b7c8a300750a1d8486bd69bfd125324eec3
Ni Chia <nichia@gmail.com>
2017-11-15 17:27:03 +0000

    Done.

---
+++ Rakefile
@@ -0,0 +1,19 @@
+# Testing => rake console
+
+require_relative './config/environment'
+require 'pry'
+require 'rake'
+
+puts "Welcome to My Pets ... object relations"
+
+def reload!
+  load './lib/cat.rb'
+  load './lib/dog.rb'
+  load './lib/fish.rb'
+  load './lib/owner.rb'
+end
+
+desc "A Console"
+task :console do
+  pry.start
+end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,69 @@
 class Owner
   # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@pets = {} #hash of pets
+  @@all = []  #array of all owners
+
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+
+  def walk_dogs
+    @pets[:dogs].collect { |d| d.mood = "happy"}
+  end
+
+  def play_with_cats
+    @pets[:cats].collect { |d| d.mood = "happy"}
+  end
+
+  def feed_fish
+    @pets[:fishes].collect { |d| d.mood = "happy"}
+  end
+
+  def sell_pets
+    @pets.each do |type, pets|
+      pets.each { |p| p.mood = "nervous"}
+    end
+    @pets.clear
+  end
+
+  def list_pets
+    if @pets.count > 0
+      "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+    else
+      "I don't have any pets"
+    end
+  end
 end
